85ec8f17e79a8fe1a4e60766a193d66e43f22312
rohilvagarwal <rohilvagarwal@gmail.com>
2023-11-01 14:48:01 -0400

    updated menu and about me

--- ExtraPages/AboutMe.py
+++ ExtraPages/AboutMe.py
@@ -8,6 +8,15 @@ scaledRoj = pygame.transform.scale(roj, (rojWidth, rojWidth)).convert_alpha()
 def draw_about_me(screen):
 	screen.blit(scaledRoj, (SCREEN_WIDTH / 2 - rojWidth / 2, 50))
 	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 70, 20, "By Rohil Agarwal")
-	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100, 20, "I go by roj.")
+	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100, 20,
+					 "This Physics Visualization App is an innovative tool designed to enhance your understanding")
+	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100 + 20, 20,
+					 "and visualization of concepts in Physics I. The app provides real-time simulations that allow")
+	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100 + 40, 20,
+					 "you to interact with physics concepts and observe their behavior as if you were conducting")
+	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100 + 60, 20,
+					 " real experiments. Whether you're a student, educator, or just someone curious about physics,")
+	draw_text_center(screen, SCREEN_WIDTH / 2, rojWidth + 100 + 80, 20,
+					 "this app is your gateway to learn more in an interesting way!")
 	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, 20, "Github: https://github.com/rohilvagarwal")
 	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT - 25, 20, "LinkedIn: https://www.linkedin.com/in/rohil-ag/")
--- ExtraPages/Menu.py
+++ ExtraPages/Menu.py
@@ -8,12 +8,12 @@ def menu_button(centerX, centerY, text):
 
 def draw_menu(screen, update_game_state):
 	#draw title
-	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 10, 70, "PhysicsStudy")
+	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 10, 70, "PhysiFun")
 	draw_text_center(screen, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 5, 30, "By Rohil Agarwal")
 
 	kinematics = menu_button(200, 2 * SCREEN_HEIGHT / 6, "Kinematics")
 	circularMotion = menu_button(200, 3 * SCREEN_HEIGHT / 6, "Circular Motion")
-	aboutMe = menu_button(200, 4 * SCREEN_HEIGHT / 6, "About Me")
+	aboutMe = menu_button(200, 4 * SCREEN_HEIGHT / 6, "Learn More")
 
 	#draw button and check if clicked
 	if kinematics.draw_and_check_click(screen):
